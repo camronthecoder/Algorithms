c00fcfe6a40d55f466eb6e8736e063c3e74068a0
camronthecoder <106614872+camronthecoder@users.noreply.github.com>
2022-07-27 15:23:22 -0400

    .

--- Set2/Reverse.java
+++ Set2/Reverse.java
@@ -1,19 +1,17 @@
-//package Algorithims.Set2;
-//
-//import java.awt.*;
-//
-//public class Reverse {
-//    ("the dog went outside");
-////    }
-////}
-//public static String reverseString (String myString){
-//char ch;
-//String reversedString = "";
-//    for (int i = 0; i < myString.length(); i++) {
-//        ch = myString.charAt(1);
-//        reversedString = ch + reversedString;
-//    }
-//    return reversedString;
-//
-//    }
-//}
+package Algorithims.Set2;
+
+public class Reverse {
+    public static void main(String[] args) {
+        System.out.println(reverseString("my Dog"));
+    }
+    public static String reverseString (String myString) {
+        String reversedString= "";
+        char ch;
+        for (int i = 0; i < myString.length(); i++) {
+            ch = myString.charAt(i);
+            reversedString = ch + reversedString;
+
+        }
+        return reversedString;
+    }
+}
--- Set3/getWordCount.java
+++ Set3/getWordCount.java
@@ -1,20 +1,19 @@
 package Algorithims.Set3;
 
-//public class getWordCount {
-//    public static void main(String[] args) {
-//
-//        System.out.println(WordCount("The dog crossed the Road"));
-//
-//    }
-//    public static int WordCount(String Scentance) {
-//        int i = 1;
-//        for (i = 0; i < Scentance.length(); i++) {
-//
-//            if (Scentance[i] = " ") {
-//                int i = i + 1;
-//            }
-//        }
-//        return i;
-//
-//    }
-//}
+public class getWordCount {
+    public static void main(String[] args) {
+        System.out.println("There Are " + WordCount("The dog crossed the Road") + " spaces in the scentence.");
+    }
+
+    public static int WordCount(String sentance) {
+        int wordCount= 0 ;
+        char[] myStringArray= sentance.toCharArray();
+        for (int i = 0; i < myStringArray.length; i++) {
+            if (myStringArray[i] == ' ') {
+                wordCount++;
+            }
+        }
+        return wordCount;
+
+    }}
+
